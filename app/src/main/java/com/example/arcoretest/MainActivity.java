990e19b0ab782aaa7d3578416e5b173df2170dfa
Shuayb Khan <shuaybk@gmail.com>
2020-05-26 11:34:28 -0400

    Add test model (Rooster) to the scene

--- app/src/main/java/com/example/arcoretest/MainActivity.java
+++ app/src/main/java/com/example/arcoretest/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.arcoretest;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
@@ -17,9 +18,11 @@ import com.google.ar.core.HitResult;
 import com.google.ar.core.Plane;
 import com.google.ar.core.exceptions.UnavailableDeviceNotCompatibleException;
 import com.google.ar.core.exceptions.UnavailableUserDeclinedInstallationException;
+import com.google.ar.sceneform.AnchorNode;
 import com.google.ar.sceneform.rendering.ModelRenderable;
 import com.google.ar.sceneform.ux.ArFragment;
 import com.google.ar.sceneform.ux.BaseArFragment;
+import com.google.ar.sceneform.ux.TransformableNode;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -56,8 +59,25 @@ public class MainActivity extends AppCompatActivity {
         arFragment.setOnTapArPlaneListener((hitResult, plane, motionEvent) -> {
             Anchor anchor = hitResult.createAnchor();
 
-            
+            ModelRenderable.builder()
+                    .setSource(this, Uri.parse("Rooster(1).sfb"))
+                    .build()
+                    .thenAccept(modelRenderable -> addModelToScene(anchor, modelRenderable))
+                    .exceptionally(throwable -> {
+                        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                        builder.setMessage(throwable.getMessage()).show();
+                        return null;
+                    });
         });
+    }
+
+    private void addModelToScene(Anchor anchor, ModelRenderable modelRenderable) {
+        AnchorNode anchorNode = new AnchorNode(anchor);
+        TransformableNode transformableNode = new TransformableNode(arFragment.getTransformationSystem());
+        transformableNode.setParent(anchorNode);
+        transformableNode.setRenderable(modelRenderable);
+        arFragment.getArSceneView().getScene().addChild(anchorNode);
+        transformableNode.select();
     }
 
     @Override
